279af65d184b1acd1425f7856171784e90ebb3d7
alikazemi7204 <zistfa@gmail.com>
2020-05-24 13:15:15 +0430

    my new

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace newProjectTest
         {
 		services.AddMvc();
             services.AddControllersWithViews();
+services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
